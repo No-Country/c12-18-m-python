d299e8c9b1bb10bc8d1669df6bea1570aebb5623
yessicarm <89185291+yessicarm@users.noreply.github.com>
2023-07-11 19:40:23 -0700

    Banner up

--- frontend/src/components/Bannerup.jsx
+++ frontend/src/components/Bannerup.jsx
@@ -1,31 +1,33 @@
-"use client";
+'use client'
 import { Typography } from "@material-tailwind/react";
 
-export default function Bannerup({ page }) {
-  var textComponent;
+export default function Bannerup({page}){
+    var textComponent;
 
-  function kindOfBanner() {
-    if (page == "services" || page == "service") {
-      textComponent = "Find a Service For You";
+    function kindOfBanner() {
+      if (page == "services" || page == "service") {
+        textComponent = "Find a Service For You";
+      }
+      if (page == "about") {
+        textComponent = "The big story behind, our beautyness center";
+      }
+      if (page == "contact") {
+        textComponent = "We Are Ready To Assist You In 24x7";
+      }
+      if (page == "login") {
+        textComponent = "Login / Signup";
+      }
+      return textComponent;
     }
-    if (page == "about") {
-      textComponent = "The big story behind, our beautyness center";
-    }
-    if (page == "contact") {
-      textComponent = "We Are Ready To Assist You In 24x7";
-    }
-    if (page == "login") {
-      textComponent = "Login / Signup";
-    }
-    return textComponent;
-  }
-  textComponent = kindOfBanner(page);
+    textComponent = kindOfBanner(page);
+    
 
-  return (
-    <header className="h-full w-full flex justify-center bg-[#422A3C] text-white p-20 ">
-      <Typography variant="h2" className="font-playfair text-center ">
-        {textComponent}
-      </Typography>
-    </header>
-  );
+    return(
+        <header className="h-full w-full flex justify-center bg-[#422A3C] text-white p-20 ">
+            <Typography variant="h2" className="font-playfair text-center ">
+          {textComponent}
+          </Typography>          
+           
+        </header>
+    )
 }
