b476426dcc62620c641e5f9b1a07319f25834756
yessicarm <89185291+yessicarm@users.noreply.github.com>
2023-07-11 18:43:13 -0700

    Bannerup con renderizado condicional

--- frontend/src/app/(publics)/services/page.jsx
+++ frontend/src/app/(publics)/services/page.jsx
@@ -2,10 +2,12 @@
 import IconsServices from "../../../components/IconsServices";
 import CardsServices from "@/components/CardsServices";
 import ImgGalery from "@/components/ImgGalery";
+import Bannerup from "@/components/Bannerup";
 
 export default function Services() {
   return (
     <>
+    <Bannerup page="services"/>
       <IconsServices />
       <CardsServices />
       <ImgGalery />
