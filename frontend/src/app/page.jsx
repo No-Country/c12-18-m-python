4ecd404174ed0a715e40aa8ea8e0dd157a8cea1f
MDCIrony <miguel.acastillodiaz@outlook.es>
2023-07-15 04:04:06 -0500

    [REFACT] Added bg-white color for main in principal layout

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,12 +1,39 @@
-import PrincipalCarousel from "@/components/PrincipalCarousel";
+import { Carousel } from "@/clientComponents/TailwindComponents";
+import CarouselElement from "@/components/CarouselElement";
 import IconsServices from "@/components/IconsServices";
 import CardsServices from "@/components/CardsServices";
 import ImgGalery from "@/components/ImgGalery";
 
+const carouselArray = [
+  {
+    src: "https://res.cloudinary.com/dbmqhlv4o/image/upload/v1689088670/Group_35279_2_drg3jo.png",
+    alt: "Imagen 1",
+    preText: "Hair Salon, wax , Beauty Salon",
+    title: "Find a service for you",
+    body: "There are many variation of passages are Ipsum available, majority have suffered alteration in some form.",
+    textButton: "Book Now",
+  },
+  {
+    src: "https://res.cloudinary.com/dbmqhlv4o/image/upload/v1689088117/carrusel_f7ip1e.png",
+    alt: "Imagen 2",
+    preText: "",
+    title: "Exclusive servises for you",
+    body:
+      "We offer a complete range of beauty services to fit your needs. From hair styling, " +
+      "to facial skin care, to body massage, manicures, pedicures and more, we are your all-in-one " +
+      "resource for a beautiful new you come and experience it today!",
+    textButton: "Book Now",
+  },
+];
+
 export default function Home() {
   return (
-    <main className="">
-      <PrincipalCarousel />
+    <main className="bg-white">
+      <Carousel autoplay loop>
+        {carouselArray.map((carouselElement) => {
+          return <CarouselElement key={carouselElement.alt} carouselElement={carouselElement} />;
+        })}
+      </Carousel>
       <IconsServices />
       <CardsServices />
       <ImgGalery />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,58 +1,58 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
-const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       gridTemplateRows: {
-          '[auto,auto,1fr]': 'auto auto 1fr',
-        }
-      
+        "[auto,auto,1fr]": "auto auto 1fr",
+      },
     },
     screens: {
-      'sm': '640px',
+      sm: "640px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
     },
     colors: {
-      'pink': '#f48fb1',
-      'purple': '#422A3C',
-      'light-pink': '#ff49db',
-      'dark': '#141414',
-      'green': '#13ce66',
-      'yellow': '#F7E5C1',
-      'gray-dark': '#555555',      
+      pink: "#f48fb1",
+      purple: "#422A3C",
+      "light-pink": "#ff49db",
+      dark: "#141414",
+      green: "#13ce66",
+      yellow: "#F7E5C1",
+      "gray-dark": "#555555",
     },
     fontFamily: {
-      inder:['Inder', 'sans-serif'],
-      manrope:['Manrope', 'sans-serif'],
-      playfair:['Playfair Display', 'serif'], 
-      poppins:['Poppins', 'sans-serif'],
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+      inder: ["Inder", "sans-serif"],
+      manrope: ["Manrope", "sans-serif"],
+      playfair: ["Playfair Display", "serif"],
+      poppins: ["Poppins", "sans-serif"],
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
     },
   },
   plugins: [],
