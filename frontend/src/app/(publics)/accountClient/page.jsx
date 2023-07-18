fe29e17d0b1420c4b5d1f7649a82ac2ac4a6ca04
RaulAJaimes <albertojara@ufps.edu.co>
2023-07-18 18:38:32 -0500

    field accountClient  full responsive with account and filters and formClient and CardClient

--- frontend/src/app/(publics)/accountClient/page.jsx
+++ frontend/src/app/(publics)/accountClient/page.jsx
@@ -1,22 +1,26 @@
 import AccountMax640 from "@/components/AccountMax640";
 import AvatarClient from "@/components/AvatarClient";
+import CardClient from "@/components/CardClient";
 import FiltersClientAllServices from "@/components/FiltersClientAllServices";
 import FormClient from "@/components/FormClient";
-import NewCard from "@/components/NewCard";
 
 export default function Account() {
   return (
     <>
-      <div className="md:flex " >
+      <div className="md:flex ">
         <div className="flex md:w-auto md:block">
           <AccountMax640 />
           <FiltersClientAllServices />
         </div>
-        <div className="md:w-full" >
-          <FormClient />
-          <NewCard/>
+        <div className="md:w-full grid md:flex flex-wrap">
+          {/*<FormClient /> */}
+          <CardClient />
+          <CardClient />
+          <CardClient />
+          <CardClient />
+          <CardClient />
+          <CardClient />
         </div>
-
       </div>
     </>
   );
