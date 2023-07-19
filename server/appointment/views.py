6c5f13acda629fc08c0a3ca936b6be8d7e91ffc4
Tomi <leaniztomas@@hotmail.com>
2023-07-19 20:16:42 -0300

    [FEAT] Filtro por horas y se visualizan unicamente los turnos disponibles para el usuario

--- server/appointment/views.py
+++ server/appointment/views.py
@@ -3,10 +3,10 @@ from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from user.models import User
 from services.models import Service
-from .models import Appointment
+from .models import Appointment, TIMETABLE
 from rest_framework import status
 from .serializers import AppointmentSerializer
-
+from collections import Counter
 
 @api_view(['POST'])
 def CreateAppointment(request):
@@ -17,7 +17,7 @@ def CreateAppointment(request):
     if serializer.is_valid():
         service = Service.objects.get(id=service_id)
         timetable = data.get('timetable')
-        # Verificar si ya hay dos citas para el mismo servicio y turno
+        # check if the service at that time has available appointments
         existing_appointments = Appointment.objects.filter(service=service, timetable=timetable).count()
         if existing_appointments >= 2:
             return Response({'message': 'Maximum appointments reached for this service and timetable.'}, status=status.HTTP_400_BAD_REQUEST)
@@ -38,7 +38,7 @@ def ListAppointments(request):
 
 @api_view(['GET'])
 def UserAppointments(request):
-    """Recieved the request as query parameters
+    """Receieved the request as query parameters
         to return a list of appointments by user"""
     user_id = request.query_params.get('id')
     try:
@@ -48,6 +48,27 @@ def UserAppointments(request):
     except User.DoesNotExist:
         raise ValidationError('User Does not exist')
 
+
 @api_view(['GET'])
 def AvailableHours(request):
-    """"""
+    hours = {
+        "1": "08:00 to 08:30", "2": "08:30 to 09:00", "3": "09:00 to 09:30", "4": "09:30 to 10:00",
+        "5": "10:00 to 10:30", "6": "10:30 to 11:00", "7": "11:00 to 11:30", "8": "11:30 to 12:00",
+        "9": "12:00 to 12:30", "10": "12:30 to 01:00", "11": "01:00 to 01:30", "12": "01:30 to 02:00",
+        "13": "02:00 to 02:30", "14": "02:30 to 03:00", "15": "03:00 to 03:30", "16": "03:30 to 04:00",
+        "17": "04:00 to 04:30", "18": "04:30 to 05:00"
+    }
+
+    service_id = request.query_params.get('service')
+    day = request.query_params.get('day')
+
+    appointments = Appointment.objects.filter(day=day, service_id=service_id)
+
+    timetable_values = appointments.values_list('timetable', flat=True)
+    timetable_list = list(timetable_values)
+
+    repeated_values = [value for value, count in Counter(timetable_list).items() if count > 1]
+    copy = hours.copy()
+    for key in repeated_values:
+        copy.pop(key, None)
+    return Response(copy, status=201)
