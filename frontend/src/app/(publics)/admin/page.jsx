16d578e28ff3640f1a7d2cd138de9a69388c7021
loli178aliberti_lorena@yahoo.com.ar <you@example.com>
2023-07-21 17:15:33 -0300

    se creo la ruta admin

---
+++ frontend/src/app/(publics)/admin/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+import Login from "@/components/Login";
+import Bannerup from "@/components/Bannerup";
+import Register from "@/components/Register";
+
+export default function Signin() {
+  return (
+    <div className="justify-center ">
+      <h1>Ruta admin</h1>
+    </div>
+  );
+}
